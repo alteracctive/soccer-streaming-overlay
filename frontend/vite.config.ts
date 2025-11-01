826dea95ea7884668b8b1ffc36880164b807901a
alteracctive <hoangnghiahuy@tamu.edu>
2025-11-01 14:42:43 -0500

    hotfix before deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,12 +5,11 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        // Your original entry point (the overlay)
+        // Control Panel
         main: resolve(__dirname, 'index.html'),
         
-        // Your new entry point (the control panel)
-        // Update this path to point inside the control_panel folder
-        controlPanel: resolve(__dirname, 'control_panel/index.html'), 
+        // Overlay
+        overlay: resolve(__dirname, 'overlay/index.html'), 
       },
     },
   },
